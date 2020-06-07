d2f36aeeddba771656668e2c484e3e6b26f979cf
jesusquezada <2016quezadaj@gmail.com>
2020-06-06 23:40:52 -0700

    Testing Soundclouds recording code snippet

--- js/soundcloud.js
+++ js/soundcloud.js
@@ -7,3 +7,13 @@ SC.initialize({
   client_id: 'YOUR_CLIENT_ID',
   redirect_uri: 'https://example.com/callback'
 });
+
+// Recording
+// The SDK uses getUserMedia, Web Workers and the Web Audio API to record from a user's computer, so make sure to check if the user's browser supports all of it (1, 2, 3) before you start recording. This example will record 5 seconds and play it back right away:
+var recorder = new SC.Recorder();
+recorder.start();
+
+setTimeout(function(){
+  recorder.stop();
+  recorder.play();
+}, 5000);
